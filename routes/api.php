9359192f6bdc507a4385a825de9599efacb8ae92
Wadhah Essam Mac <wadah-wleed@hotmail.com>
2019-01-30 02:23:07 +0300

    ending the operation like nothing happend

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::post('/compileJava', function (Request $request) {
     $rand = rand(0,99999999999);
+    mkdir('compiledFiles');
     mkdir('compiledFiles/'.$rand);
     $javaFile = fopen('compiledFiles/'.$rand.'/Test.java', 'w');
     fwrite($javaFile, $request->code);
@@ -32,22 +33,14 @@ Route::post('/compileJava', function (Request $request) {
 
 // remove files and folders inside a  directory
 function deleteAll($str) {
-    //It it's a file.
     if (is_file($str)) {
-        //Attempt to delete it.
         return unlink($str);
     }
-    //If it's a directory.
     elseif (is_dir($str)) {
-        //Get a list of the files in this directory.
         $scan = glob(rtrim($str,'/').'/*');
-        //Loop through the list of files.
         foreach($scan as $index=>$path) {
-            //Call our recursive function.
             deleteAll($path);
         }
-        //Remove the directory itself.
-        // return @rmdir($str);
-        return;
+        return @rmdir($str);
     }
 }
